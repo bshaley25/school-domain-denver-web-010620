37708e81e596b157e56bafdac317fc789a4932dc
Bshaley25 <bshaley25@gmail.com>
2020-01-13 08:29:59 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,55 @@
 # code here!
+require 'pry'
+class School
+
+    attr_accessor :roster
+    attr_reader :name
+
+    def initialize name
+        @name = name
+        @roster = {}
+    end
+
+    def add_student name, grade
+        if self.roster[grade] == nil
+            self.roster[grade] = [name]
+        else
+            self.roster[grade] << name
+        end
+    end
+
+    def grade grade
+        self.roster[grade]
+    end
+
+    # def sort
+    #     self.roster.each do |key, value|
+    #         new_array = self.roster[key]
+    #         self.roster[key] = new_array.sort
+    #     end
+    # end
+
+    def sort
+        self.roster.reduce({}) do |roster, (key,value)|
+            roster[key] = value.sort
+            roster
+        end
+    end
+
+
+
+
+end
+
+
+school = School.new("Bayside High School")
+school.add_student "c", 1
+school.add_student "b", 1
+school.add_student "a", 1
+school.add_student "f", 2
+school.add_student "e", 2
+school.add_student "d", 2
+
+#school.sort
+
+#binding.pry
